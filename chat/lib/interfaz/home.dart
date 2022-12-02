634119c9dd1fc7a835931b24cc4072d53fea33e2
AriMangata <114108201+AriMangata@users.noreply.github.com>
2022-12-01 20:45:26 -0500

    Update home.dart

--- chat/lib/interfaz/home.dart
+++ chat/lib/interfaz/home.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.indigo,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
